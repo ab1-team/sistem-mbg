e94e28a910e37c31dab703a05c15efe637708e86
arafi118 <arp46301@gmail.com>
2026-04-11 08:19:53 +0700

    Checkpoint #22

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-    }
+    public function run(): void {}
 }
